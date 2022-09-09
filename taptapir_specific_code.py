e2700b2cdbbd5536f2037a5197d6a9cc4bfbcba1
pokepetter <pokepetter@gmail.com>
2022-09-10 00:35:06 +0200

    ...

---
+++ taptapir_specific_code.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html><html lang="en"><head><meta charset="utf-8" /><title>platformer</title><link rel="stylesheet" href="../taptapir.css"></head><body></body><script src="../taptapir.js"></script>
+<script type='text/pokescript'>
+
+set_window_color('rgb(50 50 50)')
+set_background_color('rgb(9 9 1)')
+set_orientation('landscape')
+set_scale(.75)
+
+
+
+
+
+
+</script><script src="../pokescript_compiler.js"></script></html>
